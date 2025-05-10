a4762b20f34645859539d22a26ddbc4e6899bfa9
Rusvelt Jose Meza San Martín <rmezas@unal.edu.co>
2025-05-10 10:48:55 -0500

    sisas

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,8 +1,27 @@
+# ejemplo del caso de uso
+#
 
-def parse_args():
-    input_folder = sys.argv[0]
-    output_folder = sys.argv[1]
-    return output_folder, input_folder
+## wordcount.py
+
+import argparse
+import sys
+
+from homework.src._internals.parse_args import parse_args
+from homework.src._internals.read_all_lines import read_all_lines
+
+
+from homework.src._internals.parse_args import parse_args
+from homework.src._internals.read_all_lines import read_all_lines
+from homework.src._internals.count_words import count_words
+from homework.src._internals.preprocess_lines import preprocess_lines
+from homework.src._internals.split_into_words import split_into_words
+from homework.src._internals.write_word_counts import write_word_counts
+
 
 def main():
-    args = parse_args
+    input_folder, output_folder = parse_args()
+    lines = read_all_lines(input_folder)
+    preprocessed_lines = preprocess_lines(lines)
+    words = split_into_words(preprocessed_lines)
+    word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)
